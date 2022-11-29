e1dcd6676d259d36282a116910f9de3db4fb2d39
anugraha4 <96745531+anugraha4@users.noreply.github.com>
2022-11-29 10:13:30 +0530

    initial commit

---
+++ src/Substring.java
@@ -0,0 +1,18 @@
+public class Substring {
+    public static void main(String[] args) {
+        String str="Fun";
+        String str1=str.substring(0,1);
+        String str2=str.substring(0,2);
+        String str3=str.substring(1,2);
+        String str4=str.substring(2);
+        String str5=str.substring(1);
+        String str6=str.substring(0);
+        System.out.println(str1);
+        System.out.println(str2);
+        System.out.println(str3);
+        System.out.println(str4);
+        System.out.println(str5);
+        System.out.println(str6);
+
+    }
+}
